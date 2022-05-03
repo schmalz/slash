0d3b778ebd95e3829cf3294fbea8956b17ae10df
schmalz <schmalz@live.co.uk>
2022-05-03 17:26:34 +0100

    Add documentation.

--- src/sketch/dynamic.clj
+++ src/sketch/dynamic.clj
@@ -12,50 +12,55 @@
    (save-frame-to-disk)
    state))
 
-(defn- half-width
-  []
+(defn- width-proportion
+  "A proportion of the frame width."
+  [p]
   (* (q/width)
-     0.5))
+     p))
 
-(defn- half-height
-  []
+(defn- height-proportion
+  "A proportion of the frame height."
+  [p]
   (* (q/height)
-     0.5))
+     p))
 
 (defn- draw-ellipse
+  "Create a 'slash' in the circles."
   []
-  (q/with-translation [(half-width)
-                       (half-height)]
-                      (q/with-rotation [(q/random (* q/PI 2))]
-                                       (q/ellipse (q/random (- (half-width))
-                                                            (half-width))
-                                                  (q/random (- (half-height))
-                                                            (half-height))
-                                                  (q/random 799)
-                                                  (q/random 11.0)))))
+  (q/with-translation [(width-proportion 0.5)
+                       (height-proportion 0.5)]
+    (q/with-rotation [(q/random (* q/PI 2))]
+      (q/ellipse (q/random (- (width-proportion 0.5))
+                           (width-proportion 0.5))
+                 (q/random (- (height-proportion 0.5))
+                           (height-proportion 0.5))
+                 (q/random 799.0)
+                 (q/random 11.0)))))
 
 (defn- draw-ellipses
-  []
+  "Create N slashes in the circles."
+  [n]
   (q/fill 44 10 99 1.0)
   (q/stroke 44 10 99 1.0)
-  (dotimes [_ 11]
+  (dotimes [_ n]
     (draw-ellipse)))
 
 (defn- draw-circles
-  []
-  (q/with-translation [(half-width)
-                       (half-height)]
-                      (dotimes [r 535]
-                        (q/stroke 180 9 63 (q/random 1.0))
-                        (q/ellipse (q/random -5 5) (q/random -5 5) r r))))
+  "Draw N circles."
+  [n]
+  (q/with-translation [(width-proportion 0.5)
+                       (height-proportion 0.5)]
+    (dotimes [r n]
+      (q/stroke 180 9 63 (q/random 1.0))
+      (q/ellipse (q/random -5 5) (q/random -5 5) r r))))
 
 (defn draw
   []
   (q/no-loop)
   (q/background 44 10 99)
   (q/no-fill)
-  (draw-circles)
-  (draw-ellipses)
+  (draw-circles 535)
+  (draw-ellipses 11)
   (save-frame-to-disk))
 
 (defn initialise
